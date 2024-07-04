8a5bdc6ff08db5080f13119cfbaeac2b2f46daec
Arnaud <arnaud.gucciardi@toelt.ai>
2024-07-04 11:54:57 +0200

    project update and clean up

---
+++ report_generation/descriptionv5.py
@@ -0,0 +1,54 @@
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import pipeline
+from datasets import load_dataset
+from tqdm import tqdm
+import csv
+
+dataset = load_dataset("sartajbhuvaji/Brain-Tumor-Classification")
+
+model_id = "llava-hf/llava-1.5-7b-hf"
+pipe = pipeline("image-to-text", model=model_id, device="cuda:2")
+
+def descript(image, label):
+    dic_class = {0:"glioma", 1:"meningioma", 2:"no", 3:"pituitary"}
+    # image, label = example['image'], example['label']
+        
+    if dic_class[label] != 2:
+        prompt = f"USER: <image>\nGenerate a report of the displayed MRI, is it T1 or T2 weighted? In which area of the brain is located the tumor, \
+                and can you locate where the {dic_class[label]} tumor is ? \
+                Answer in the form of the sentence : This is a (T1 or T2 weighted) image of a brain with (your answer with the tumor name) tumor. It is located in (your answer).\
+                \nASSISTANT:"
+    else: 
+        prompt = f"USER: <image>\nGenerate a report of the displayed MRI, is it T1 or T2 weighted? In which area of the brain is located the tumor, \
+                and can you confirm there is no tumor? \
+                Answer in the form of the sentence : This is a (T1 or T2 weighted) image of a brain without tumor.\
+                \nASSISTANT:"
+
+    outputs = pipe(image, prompt=prompt, generate_kwargs={"max_new_tokens": 200})
+    description = outputs[0]['generated_text'].split("ASSISTANT: ", maxsplit=1)[1]
+    
+    return description
+
+
+def main():
+    # Open a CSV file to write the descriptions
+    with open('mri_descriptions.csv', 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        
+        # Write the header
+        csvwriter.writerow(['Index', 'Description'])
+        
+        # Process each example in the training set
+        for index, example in enumerate(tqdm(dataset['Training'])):
+            description = descript(example['image'], example['label'])
+            csvwriter.writerow([index, description])
+            
+            # Optional: print every 10th description to monitor progress
+            if index % 10 == 0:
+                print(f"Index {index}: {description}")
+
+    print("Descriptions have been saved to mri_descriptions.csv")
+
+if __name__ == "__main__":
+    main()
---
+++ report_generation/miniclip2.py
@@ -0,0 +1,157 @@
+import torch
+import json
+import csv
+import requests
+from PIL import Image
+from datasets import load_dataset
+from transformers import AutoModel, AutoTokenizer
+from torchvision.transforms.functional import pil_to_tensor
+from torchmetrics.multimodal.clip_score import CLIPScore
+from PIL import Image
+from transformers import BlipProcessor, BlipForImageTextRetrieval
+
+# description model
+model = AutoModel.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True, torch_dtype=torch.float16)
+model = model.to(device='cuda')
+# tokenizer
+tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True)
+model.eval()
+
+# blip model
+processor = BlipProcessor.from_pretrained("Salesforce/blip-itm-base-coco")
+blipmodel = BlipForImageTextRetrieval.from_pretrained("Salesforce/blip-itm-base-coco")
+
+dataset = load_dataset("sartajbhuvaji/Brain-Tumor-Classification")
+
+# Initialize a list to store all the reports
+# all_reports = []
+
+def generate_report(image, caption):
+    question = f'Generate a report of the MRI image presented, use this description : {caption}.'
+    msgs = [{'role': 'user', 'content': question}]
+    report = model.chat(
+        image=image,
+        msgs=msgs,
+        tokenizer=tokenizer,
+        sampling=True,
+        temperature=0.7,
+        system_prompt='Medical report style, focus on image description and conditions.'
+    )
+    
+    return report
+
+def get_clip_score(image, report):
+
+    metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
+    clip_score = metric(pil_to_tensor(image), report[:50])
+    clip_score = clip_score.detach().item()
+
+    return clip_score
+
+def get_blip_score(image, report):
+
+    inputs = processor(images=image, text=report[:50], return_tensors="pt", padding=True)
+    with torch.no_grad():
+        outputs = blipmodel(**inputs)
+        blip_score = outputs.itm_score.softmax(dim=1)[:, 1]
+    blip_score = blip_score.item()
+
+    return blip_score
+
+
+def main(input, output):
+    # 
+    all_reports = []
+    with open(input, mode='r') as input_file:
+        csvFile = csv.reader(input_file)
+        next(csvFile, None)  # skip header
+
+        for index, (image, lines) in enumerate(zip(dataset['Training']['image'], csvFile)):
+            res = generate_report(image, lines[1])
+
+            ## SCORES
+            clip_score = get_clip_score(image, res)
+            blip_score = get_blip_score(image, res)
+
+            # Create a dictionary for this report
+            report = {
+                "Index": lines[0],
+                "Description": lines[1],
+                "Generated_Report": res,
+                "CLIP_Score": clip_score,
+                "BLIP_Score": blip_score
+            }
+
+            # Add the report to our list
+            all_reports.append(report)
+
+            # prints
+            print(f"Index {lines[0]}")
+            print(f"Description {res}")
+            print(f"CLIP Score {clip_score}")
+            print(f"BLIP Score {blip_score}")
+
+    # Save all reports to a JSON file
+    with open(output, 'w') as json_file:
+        json.dump(all_reports, json_file, indent=4)
+
+    print("All reports have been saved to mri_reports.json")
+
+# # Reading the short descriptions
+# with open('mri_descriptions.csv', mode='r') as input_file:
+#     csvFile = csv.reader(input_file)
+#     next(csvFile, None)  # skip header
+
+#     for index, (image, lines) in enumerate(zip(dataset['Training']['image'], csvFile)):
+#         question = f'Generate a report of the MRI image presented, use this description : {lines[1]}.'
+#         msgs = [{'role': 'user', 'content': question}]
+#         res = model.chat(
+#             image=image,
+#             msgs=msgs,
+#             tokenizer=tokenizer,
+#             sampling=True,
+#             temperature=0.7,
+#             system_prompt='Medical report style, focus on image description and conditions.'
+#         )
+
+#         ## CLIP SCORE
+#         metric = CLIPScore(model_name_or_path="openai/clip-vit-base-patch16")
+#         clip_score = metric(pil_to_tensor(image), res[:50])
+#         clip_score = clip_score.detach().item()
+
+#         ## BLIP SCORE
+#         inputs = processor(images=image, text=res[:50], return_tensors="pt", padding=True)
+#         with torch.no_grad():
+#             outputs = blipmodel(**inputs)
+#             blip_score = outputs.itm_score.softmax(dim=1)[:, 1]
+#         blip_score = blip_score.item()
+
+#         # Create a dictionary for this report
+#         report = {
+#             "Index": lines[0],
+#             "Description": lines[1],
+#             "Generated_Report": res,
+#             "CLIP_Score": clip_score,
+#             "BLIP_Score": blip_score
+#         }
+
+#         # Add the report to our list
+#         all_reports.append(report)
+
+#         # prints
+#         print(f"Index {lines[0]}")
+#         print(f"Description {res}")
+#         print(f"CLIP Score {clip_score}")
+#         print(f"BLIP Score {blip_score}")
+
+# # Save all reports to a JSON file
+# with open('mri_reports.json', 'w') as json_file:
+#     json.dump(all_reports, json_file, indent=4)
+
+# print("All reports have been saved to mri_reports.json")
+
+
+if __name__ == "__main__":
+    input_csv = 'mri_descriptions.csv'
+    output_json = 'mri_reports.json'
+    main(input_csv, output_json)
